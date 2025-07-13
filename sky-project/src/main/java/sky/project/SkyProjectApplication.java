fc7874b2c4af2c6e808acc0548da5fede9582233
liu-sunset <liulooklandscape@outlook.com>
2025-07-13 12:23:08 +0800

    初始化

---
+++ sky-project/src/main/java/sky/project/SkyProjectApplication.java
@@ -0,0 +1,13 @@
+package sky.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProjectApplication.class, args);
+    }
+
+}
