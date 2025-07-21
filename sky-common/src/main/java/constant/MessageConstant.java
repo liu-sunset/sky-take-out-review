bddfb2effa20055fcfdc0479763277ad8907f9a2
liu-sunset <liulooklandscape@outlook.com>
2025-07-21 18:01:02 +0800

    1. 实现了全局异常处理器可以处理SQLIntegrityConstraintViolationException异常

---
+++ sky-common/src/main/java/constant/MessageConstant.java
@@ -0,0 +1,7 @@
+package constant;
+
+import java.io.Serializable;
+
+public class MessageConstant implements Serializable {
+    public static String UNKNOW_ERROR = "未知错误";
+}
--- sky-common/src/main/java/handler/GlobalExceptionHandler.java
+++ sky-common/src/main/java/handler/GlobalExceptionHandler.java
@@ -1,28 +1,34 @@
 package handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import result.Result;
+
 import java.sql.SQLIntegrityConstraintViolationException;
 
 
 @Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     /*
-    * 处理业务异常
+    * 处理所有异常
+    * mybatis会将SQLIntegrityConstraintViolationException封装成runtimeexception向上抛出
     * */
     @ExceptionHandler
     public Result exceptionHandler(RuntimeException e){
-        log.error("业务异常信息:{}",e.getMessage());
-        return Result.error(e.getMessage());
+        if (e.getCause() instanceof SQLIntegrityConstraintViolationException){
+            SQLIntegrityConstraintViolationException se = (SQLIntegrityConstraintViolationException) e.getCause();
+            String[] splits = se.getMessage().split(" ");
+            log.error("SQL异常信息:{}","已存在"+splits[2]);
+            return Result.error("已存在"+splits[2]);
+        }
+        else{
+            log.error("业务异常信息:{}", e.getMessage());
+            return Result.error(e.getMessage());
+        }
     }
-
-//    @ExceptionHandler
-//    public Result sqlExceptionHandler(SQLIntegrityConstraintViolationException e){
-//
-//    }
-
-
 }
